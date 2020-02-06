84822cdea89a9e5337305396ba5b65a1d68f00a0
Qfake <qfake.dk@gmail.com>
2020-02-06 20:45:06 +0200

    environments

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -1,13 +1,21 @@
 <?php
 return [
-    'components' => [
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
-        ],
-    ],
+	'components' => [
+		'mailer' => [
+			'class'            => 'yii\swiftmailer\Mailer',
+			'viewPath'         => '@common/mail',
+			// send all mails to a file by default. You have to set
+			// 'useFileTransport' to false and configure a transport
+			// for the mailer to send real emails.
+			'useFileTransport' => true,
+		],
+		'db'     => [
+			'class'          => 'yii\db\Connection',
+			'dsn'            => 'pgsql:host=127.0.0.1;dbname=postgres',
+			'username'       => 'postgres',
+			'password'       => '',
+			'charset'        => 'utf8',
+			'emulatePrepare' => true,
+		],
+	],
 ];
--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -5,5 +5,13 @@ return [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
         ],
+        'db'     => [
+	        'class'          => 'yii\db\Connection',
+	        'dsn'            => 'pgsql:host=127.0.0.1;dbname=postgres',
+	        'username'       => 'postgres',
+	        'password'       => '',
+	        'charset'        => 'utf8',
+	        'emulatePrepare' => true,
+        ],
     ],
 ];
